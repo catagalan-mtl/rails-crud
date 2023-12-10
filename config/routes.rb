32a1506afd5ad1a0ac21b84099ab17ea6e3f5e2f
Catalina Galan <catalinagalanvargas@gmail.com>
2023-12-10 15:57:34 -0500

    restaurant CRUD class example finished

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get 'restaurants/new', to: 'restaurants#new'
   post 'restaurants', to: 'restaurants#create'
   get 'restaurants/:id', to: 'restaurants#show', as: :'restaurant'
+  get 'restaurants/:id/edit', to: 'restaurants#edit', as: :edit_restaurant
+  patch 'restaurants/:id', to: 'restaurants#update'
+  delete 'restaurants/:id', to: 'restaurants#destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
